e558dc9dd26e2d5e1b8ddd938c794091d6a40c03
Nicholas Shiell <nicholas@nshiell.com>
2018-11-07 12:30:15 +0000

    Working log, refactored GUI

--- apache-log-viewer.py
+++ apache-log-viewer.py
@@ -1,19 +1,5 @@
 #! /usr/bin/python3
 # Character Encoding: UTF-8
-
-
-
-
-
-''' pqt_tableview3.py
-explore PyQT's QTableView Model
-using QAbstractTableModel to present tabular data
-allow table sorting by clicking on the header title
-
-used the Anaconda package (comes with PyQt4) on OS X
-(dns)
-'''
-
 #coding=utf-8
 
 import operator  # used for sorting
@@ -29,33 +15,44 @@ import pprint
 
 import sys
 
+class QTableView_Log(QTableView):
+    def __init__(self):
+        super().__init__()
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        #self.table_view.horizontalHeader().setStretchLastSection(QHeaderView.Stretch)
 
-class MyWindow(QWidget):
+        ## works?????
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        # bind cell click to a method reference
+        #self.clicked.connect(QAbstractItemView.showSelection)
+        #self.clicked.connect(QAbstractItemView.selectRow)
+
+        #self.setModel(self.table_model)
+        # enable sorting
+        self.setSortingEnabled(False)
+
+class Window_title:
+    path = None
+
+    @property
+    def text(self):
+        return '%s - Log Viewer' % self.path
+
+class Log_Window(QWidget):
+    title = None
+
     def __init__(self, dataList, header, pid, *args):
         QWidget.__init__(self, *args)
-        # setGeometry(x_pos, y_pos, width, height)
+        self.title = Window_title()
+        self.title.path = 'sfgsdfg'
         self.setGeometry(70, 150, 1326, 582)
-        self.setWindowTitle("Click on the header to sort table")
+        self.setWindowTitle(self.title.text)
 
         self.table_model = MyTableModel(self, dataList, header, pid)
-        self.table_view = QTableView()
-        #self.table_view.setSelectionMode(QAbstractItemView.SingleSelection)
-        
-        
-        
-        
-        
-        ## works?????
-        self.table_view.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.table_view.setSelectionBehavior(QAbstractItemView.SelectRows)
-        # bind cell click to a method reference
-        self.table_view.clicked.connect(self.showSelection)
-        self.table_view.clicked.connect(self.selectRow)
 
+        self.table_view = QTableView_Log()
         self.table_view.setModel(self.table_model)
-        # enable sorting
-        self.table_view.setSortingEnabled(True)
-
         layout = QVBoxLayout(self)
         layout.addWidget(self.table_view)
         self.setLayout(layout)
@@ -84,6 +81,8 @@ class MyWindow(QWidget):
         event.accept()
         os._exit(0)
         #sys.exit()
+    #def keyPressEvent(self, event):
+        #print('dfg')
 
 class MyTableModel(QAbstractTableModel):
     """
@@ -129,47 +128,12 @@ class MyTableModel(QAbstractTableModel):
             lines.append([v for k,v in line.items()])
 
         self.mylist = lines
-
+#setStyleSheet
+        #self.index(0, 0).setStyleSheet('background-color: red')
         self.layoutAboutToBeChanged.emit()
         self.dataChanged.emit(self.createIndex(0, 0), self.createIndex(self.rowCount(0), self.columnCount(0)))
         self.layoutChanged.emit()
         self.parent.table_view.scrollToBottom()
-        return None
-        dataList2 = []
-        if self.change_flag is True:
-            dataList2 = [
-                [QCheckBox("关"), 0, '063802', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '02', 'cu1705,cu1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '03', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '04', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '01', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '02', 'ru1705,ru1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '02', 'ni1705,ni1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '063802', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-            ]
-            self.change_flag = False
-        elif self.change_flag is False:
-            dataList2 = [
-                [QCheckBox("关"), 0, '058176', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '02', 'cu1705,cu1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '03', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '04', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '01', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '02', 'ru1705,ru1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '02', 'ni1705,ni1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-                [QCheckBox("关"), 0, '058176', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-            ]
-            self.change_flag = True
-
-        lines = []
-        for line in mylist.parsed_lines:
-            lines.append([v for k,v in line.items()])
-
-
-        self.mylist = dataList2
-        self.layoutAboutToBeChanged.emit()
-        self.dataChanged.emit(self.createIndex(0, 0), self.createIndex(self.rowCount(0), self.columnCount(0)))
-        self.layoutChanged.emit()
 
     def rowCount(self, parent):
         return len(self.mylist)
@@ -180,94 +144,28 @@ class MyTableModel(QAbstractTableModel):
     def data(self, index, role):
         if not index.isValid():
             return None
+
         value = self.mylist[index.row()][index.column()]
-        #if (index.column() == 0):
-        #    value = self.mylist[index.row()][index.column()].text()
-        #else:
-        #    value = self.mylist[index.row()][index.column()]
-        if role == QtCore.Qt.EditRole:
+
+        if role == QtCore.Qt.BackgroundRole:
+            if index.row() == 2:
+                return QColor(200, 255, 230)
+                #return QtGui.setStyleSheet('')
+        elif role == QtCore.Qt.EditRole:
             return value
         elif role == QtCore.Qt.DisplayRole:
             return value
-        #elif role == QtCore.Qt.CheckStateRole:
-            #if index.column() == 0:
-            #    # print(">>> data() row,col = %d, %d" % (index.row(), index.column()))
-            #    if self.mylist[index.row()][index.column()].isChecked():
-            #        return QtCore.Qt.Checked
-            #    else:
-            #        return QtCore.Qt.Unchecked
 
     def headerData(self, col, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.header[col]
         return None
 
-    def sort(self, col, order):
-        """sort table by given column number col"""
-        # print(">>> sort() col = ", col)
-        if col != 0:
-            self.emit(SIGNAL("layoutAboutToBeChanged()"))
-            self.mylist = sorted(self.mylist, key=operator.itemgetter(col))
-            if order == Qt.DescendingOrder:
-                self.mylist.reverse()
-            self.emit(SIGNAL("layoutChanged()"))
-
-    def flags(self, index):
-        if not index.isValid():
-            return None
-        # print(">>> flags() index.column() = ", index.column())
-        if index.column() == 0:
-            # return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsUserCheckable
-            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsUserCheckable
-        else:
-            return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
-
-    def setData(self, index, value, role):
-        if not index.isValid():
-            return False
-        # print(">>> setData() role = ", role)
-        # print(">>> setData() index.column() = ", index.column())
-        # print(">>> setData() value = ", value)
-        if role == QtCore.Qt.CheckStateRole and index.column() == 0:
-            print(">>> setData() role = ", role)
-            print(">>> setData() index.column() = ", index.column())
-            if value == QtCore.Qt.Checked:
-                self.mylist[index.row()][index.column()].setChecked(True)
-                self.mylist[index.row()][index.column()].setText("开")
-                # if studentInfos.size() > index.row():
-                #     emit StudentInfoIsChecked(studentInfos[index.row()])     
-            else:
-                self.mylist[index.row()][index.column()].setChecked(False)
-                self.mylist[index.row()][index.column()].setText("关")
-        else:
-            print(">>> setData() role = ", role)
-            print(">>> setData() index.column() = ", index.column())
-        # self.emit(SIGNAL("dataChanged(QModelIndex,QModelIndex)"), index, index)
-        print(">>> setData() index.row = ", index.row())
-        print(">>> setData() index.column = ", index.column())
-        self.dataChanged.emit(index, index)
-        return True
-
 def timer_func(win, mylist):
     print(">>> timer_func()")
     win.table_model.setDataList(mylist)
     win.table_view.repaint()
     win.table_view.update()
-
-# def timer_func(num):
-#     print(">>> timer_func() num = ", num)
-
-
-
-
-#match1 = re.findall('\?P\<(.*?)\>', apache_regex)
-#pprint.pprint(match1)
-
-
-#header = ['timestamp', 'type', 'PID', 'Client', 'Message']
-#for v in match.groups():
-#    print(v)
-#    print('\n\n')
 
 import time
 import subprocess
@@ -281,7 +179,6 @@ class Log_File:
         self.path = path
 
     def read_file(self):
-        # file handle fh
         fh = open(self.path)
         while True:
             line = fh.readline().replace('\n', '')
@@ -361,33 +258,18 @@ class Log_Data_Processor_Thread(threading.Thread):
             self.parsed_lines.append(self.line_parser.parse(line))
 
 line_format = Line_Format('^\[(?P<Timestamp>[^\]]+)\] \[(?P<Type>[^\]]+)\] \[(?P<PID>[^\]]+)\] (?P<Message>.*)$')
-# Make the editable
-log_file = Log_File('/var/log/apache2/error.log')
+
+log_file = Log_File(sys.argv[1])
 line_parser = Line_Parser(line_format)
 log_data_processor = Log_Data_Processor_Thread(log_file, line_parser)
 log_data_processor.start()
 
 if __name__ == '__main__':
     app = QApplication([])
-    # you could process a CSV file to create this data
-    #header = ['开关', '只平', '期货账号', '策略编号', '交易合约', '总持仓', '买持仓', '卖持仓', '持仓盈亏', '平仓盈亏', '手续费', '净盈亏', '成交量', '成交金额', 'A成交率', 'B成交率', '交易模型', '下单算法']
     header = ['timestamp', 'type', 'PID', 'Client', 'Message']
-    # a list of (fname, lname, age, weight) tuples
-    #checkbox1 = QCheckBox("关");
-    #checkbox1.setChecked(True)
-    #dataList = [
-    #    [checkbox1, 0, '058176', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '02', 'cu1705,cu1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '03', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '04', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '01', 'zn1705,zn1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '02', 'ru1705,ru1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '02', 'ni1705,ni1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #    [QCheckBox("关"), 0, '058176', '01', 'rb1705,rb1710', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'MA', '01'],
-    #]
 
     dataList = []
-    win = MyWindow(log_data_processor, line_format.fields, log_file.pid)
+    win = Log_Window(log_data_processor, line_format.fields, log_file.pid)
     win.show()
     # win.table_model.setDataList(dataList)
     # timer = threading.Timer(10, timer_func, (win, dataList2))
