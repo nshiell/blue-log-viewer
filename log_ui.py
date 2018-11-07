1695d4a566c3caf6ccd812e139fb272b49127223
Nicholas Shiell <nicholas@nshiell.com>
2018-11-07 17:38:01 +0000

    Double click now opens message box

--- log_ui.py
+++ log_ui.py
@@ -14,21 +14,53 @@ class Window_title:
     def text(self):
         return '%s - Log Viewer' % self.path
 
-class QTableView_Log(QTableView):
+class Line_QMessageBox(QMessageBox):
+    bullet = None
+
     def __init__(self):
         super().__init__()
-        self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        #self.table_view.horizontalHeader().setStretchLastSection(QHeaderView.Stretch)
+        bullet = u"\u2022"
+        self.bullet = '<br />    <span style="color: #AAAAAA">' + bullet + '</span> '
 
-        ## works?????
+    def list_to_bullets(self, list_details):
+        bullet_list = self.bullet.join(list_details)
+        return self.bullet + bullet_list
+
+    def set_line(self, parsed_line, row_no):
+        self.setIcon(QMessageBox.Information)
+        self.setText("Information about the item")
+        self.setInformativeText(self.list_to_bullets(parsed_line[:-1]))
+        self.setWindowTitle('Line %d Details' % row_no)
+        self.setDetailedText(parsed_line[-1])
+        self.setStandardButtons(QMessageBox.Cancel)
+
+class Events:
+    table_model = None
+
+    def __init__(self, table_model):
+        self.table_model = table_model
+
+    def table_double_click(self, modeIndex):
+        msg = Line_QMessageBox()
+        msg.set_line(
+            self.table_model.parsed_lines[modeIndex.row()],
+            modeIndex.row())
+        msg.exec_()
+
+    def window_close(self):
+        os._exit(0)
+
+class QTableView_Log(QTableView):
+    def __init__(self, events):
+        super().__init__()
+        self.events = events
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.setSelectionMode(QAbstractItemView.SingleSelection)
         self.setSelectionBehavior(QAbstractItemView.SelectRows)
 
-        # bind cell click to a method reference
-        #self.clicked.connect(QAbstractItemView.showSelection)
-        #self.clicked.connect(QAbstractItemView.selectRow)
-
+        self.doubleClicked.connect(self.events.table_double_click)
         self.setSortingEnabled(False)
+
     def setColumsHeaderWidths(self):
         header = self.horizontalHeader()
         columnCount = header.count()
@@ -38,21 +70,23 @@ class QTableView_Log(QTableView):
                 header.setSectionResizeMode(column, QHeaderView.Interactive)
             else:
                 header.setSectionResizeMode(column, QHeaderView.Stretch)
-    
 
 class Window(QWidget):
-    title = None
+    events = None
 
     def __init__(self, log_data_processor, header, *args):
         QWidget.__init__(self, *args)
+
         self.title = Window_title()
         self.title.path = log_data_processor.log_file.path
         self.setGeometry(70, 150, 1326, 582)
         self.setWindowTitle(self.title.text)
 
         self.table_model = LogTableModel(self, log_data_processor)
+        
+        self.events = Events(self.table_model)
 
-        self.table_view = QTableView_Log()
+        self.table_view = QTableView_Log(self.events)
         self.table_view.setModel(self.table_model)
         self.table_view.setColumsHeaderWidths()
 
@@ -66,17 +100,6 @@ class Window(QWidget):
         self.table_view.setModel(self.table_model2)
         self.table_view.update()
 
-    def showSelection(self, item):
-        cellContent = item.data()
-        # print(cellContent)  # test
-        sf = "You clicked on {}".format(cellContent)
-        # display in title bar for convenience
-        self.setWindowTitle(sf)
-
-    def selectRow(self, index):
-        # print("current row is %d", index.row())
-        pass
-
     def closeEvent(self, event):
         event.accept()
-        os._exit(0)
+        self.events.window_close()
