9aa79cc6f5c97b2e08afdda5ea57ccc41c39ea37
Nicholas Shiell <nicholas@nshiell.com>
2018-12-17 19:00:14 +0000

    Refactor - subclassed QApplication

--- apache-log-viewer.py
+++ apache-log-viewer.py
@@ -6,29 +6,36 @@ from log_ui import Window
 from log_poller import Line_Format, File, Line_Parser, Processor_Thread
 from argparse import ArgumentParser
 
-parser = ArgumentParser(
-    description='Show an autoupdating grid from Apache\'s Error Log'
-)
-parser.add_argument("file", help="The Path of the log file to tail", nargs='?')
+class QApplication_Apache_Log_viewer(QApplication):
+    args = None
 
-parser.add_argument('--is-dark', action='store_const',
-                    required=False,
-                    const=True,
-                    help='Whether to use dark colours (for dark themes)')
+    def __init__ (self, argv, args):
+        self.args = args
+        super().__init__(argv)
 
-args = parser.parse_args()
+    def exec_(self):
+        line_format = Line_Format(
+            #  [some date]                [:error]            [pid - ignored] [client xxx.xxx.xxx.xxx] message to EOL
+            '^\[(?P<Timestamp>[^\]]+)\] \[(?P<Type>[^\]]+)\] \[(?:[^\]]+)\] \[client (?P<Client>[^\]]+)\] (?P<Message>.*)$')
 
-line_format = Line_Format(
-    #  [some date]                [:error]            [pid - ignored] [client xxx.xxx.xxx.xxx] message to EOL
-    '^\[(?P<Timestamp>[^\]]+)\] \[(?P<Type>[^\]]+)\] \[(?:[^\]]+)\] \[client (?P<Client>[^\]]+)\] (?P<Message>.*)$')
+        log_file = File(self.args.file)
+        line_parser = Line_Parser(line_format)
+        log_data_processor = Processor_Thread(log_file, line_parser)
+        log_data_processor.start()
 
-log_file = File(args.file)
-line_parser = Line_Parser(line_format)
-log_data_processor = Processor_Thread(log_file, line_parser)
-log_data_processor.start()
+        Window(log_data_processor, line_format.fields, self.args.is_dark).show()
+
+        super().exec_()
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    parser = ArgumentParser(
+        description='Show an autoupdating grid from Apache\'s Error Log'
+    )
+    parser.add_argument("file", help="The Path of the log file to tail", nargs='?')
 
-    Window(log_data_processor, line_format.fields, args.is_dark).show()
-    app.exec_()
+    parser.add_argument('--is-dark', action='store_const',
+                        required=False,
+                        const=True,
+                        help='Whether to use dark colours (for dark themes)')
+
+    QApplication_Apache_Log_viewer(sys.argv, parser.parse_args()).exec_()
