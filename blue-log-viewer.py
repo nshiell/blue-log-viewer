2fefc6fdf01d8b47f6542685b2f534c2f11115dc
Nicholas Shiell <nicholas@nshiell.com>
2020-04-28 13:48:06 +0100

    renamed: blue-log-viewer1.py -> blue-log-viewer.py

--- blue-log-viewer.py
+++ blue-log-viewer.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 """
-    Apache Log Viewer - View Apache's Log in a GUI
+    Blue Log Viewer - Log files in a GUI
     This program is not related to Apache or the Apache Software Foundation in any way
     Copyright (C) 2019  Nicholas Shiell
 
@@ -19,11 +19,33 @@
 """
 import sys
 from PyQt5.QtWidgets import QApplication
-from log_ui import Window_Factory
 from argparse import ArgumentParser
-from line_format import Factory as LineFormatFactory
+from blueLogViewer.windows import QMainWindowBlueLogViewer, get_valid_path
+from blueLogViewer import EventsBinder
+from blueLogViewer import LineCollection, LineCollectionBroker
+from test import server
 
 import signal
+
+from PyQt5.QtCore import QRunnable, pyqtSlot, QThreadPool
+
+import time
+
+class Worker(QRunnable):
+    '''
+    Worker thread
+    '''
+    main_window = None
+
+    def __init__(self, main_window, *args, **kwargs):
+        super().__init__()
+        self.args = args
+        self.kwargs = kwargs
+        self.main_window = main_window
+
+    @pyqtSlot()
+    def run(self):
+        server.start(self.main_window)
 
 if __name__ == '__main__':
     # Kill the app on ctrl-c
@@ -43,8 +65,27 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     args = parser.parse_args()
 
-    window = Window_Factory().create(LineFormatFactory(), args)
-    window.show()
-    window.start_polling()
+    # We need a main window before we show anything
+    # even if we are not ready to show it yet!
+    main_window = QMainWindowBlueLogViewer()
+    path = get_valid_path(main_window, args.file)
+
+    broker = LineCollectionBroker(path, main_window)
+    broker.read_file_current_lines()
+    broker.start_tailling()
+
+    main_window.setup(broker.table_model)
+    EventsBinder(main_window, broker.log_file)
+    main_window.show()
+
+    # Needed to allow the window to be drawn to get it's colors
+    app.processEvents()
+    main_window.is_now_visibile.emit()
+
+    # Flip this to true to enable functional testing
+    if False:
+        worker = Worker(main_window)
+        threadpool = QThreadPool()
+        threadpool.start(worker)
 
     app.exec_()
