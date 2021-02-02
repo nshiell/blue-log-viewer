200639f6956f356e0f2021e0d3436d8b49b1f263
Nicholas Shiell <nicholas@nshiell.com>
2021-02-02 20:35:02 +0000

    Icon svg to png converter

--- tools/icon-svg-to-png.sh
+++ tools/icon-svg-to-png.sh
@@ -11,6 +11,14 @@ if [[ ! -d "svg-to-png-using-chromium" ]]; then
     git clone git@github.com:nshiell/svg-to-png-using-chromium.git svg-to-png-using-chromium
 fi
 
-docker run -it -v "$PWD/svg-to-png-using-chromium:/usr/src/app" -v "$PWD/../artwork/icon.svg:/usr/src/app/icon.svg" markadams/chromium-xvfb-py3 bash -c "pip3 install pillow && python3 ./svg-to-png-using-chromium.py --width 256 icon.svg"
+docker run -it \
+    -v "$PWD/svg-to-png-using-chromium:/usr/src/app" \
+    -v "$PWD/../artwork/icon.svg:/usr/src/app/icon.svg" \
+    markadams/chromium-xvfb-py3 bash -c \
+        "pip3 install pillow && python3 ./svg-to-png-using-chromium.py --width 256 icon.svg && chown $(id -u ${USER}):$(id -g ${USER}) icon.png"
 
-mv --force svg-to-png-using-chromium/icon.png ../artwork/blue-log-viewer.png
+mv svg-to-png-using-chromium/icon.png ../artwork/blue-log-viewer.png
+
+echo -ne "\e[32m" # green text
+echo -ne "Created png: $(realpath $PWD/../artwork/blue-log-viewer.png)"
+echo -e "\e[0m"
